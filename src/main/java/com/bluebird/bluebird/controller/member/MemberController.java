bc9403ceaf047c91e07014412ecc1b318df67292
nhj <nhj6336@gmail.com>
2024-01-17 13:22:21 +0900

    회원가입jpa 연결

--- src/main/java/com/bluebird/bluebird/controller/member/MemberController.java
+++ src/main/java/com/bluebird/bluebird/controller/member/MemberController.java
@@ -1,14 +1,16 @@
 package com.bluebird.bluebird.controller.member;
 
+
 import com.bluebird.bluebird.domain.Member;
 import com.bluebird.bluebird.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
+@Slf4j
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor //생성자 주입
@@ -16,13 +18,26 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/regist")
-    public String registerForm(){
-        return "memberRegist";
+
+    @GetMapping
+    public String members(){
+        return "/member/login";
     }
 
-    @PostMapping("/regist")
-    public String register(Member member, RedirectAttributes redirectAttributes){
-        return "redirect:/";
+    @GetMapping("/signup")
+    public String signUp(){
+        return "/member/memberSignup";
+    }
+
+    @PostMapping("/signup")
+    public String signUp(@ModelAttribute("member") Member member, RedirectAttributes redirectAttributes){
+
+        log.trace("trace log={}", member.toString());
+        log.debug("debug log={}", member.toString());
+        log.info("info log={}", member.toString());
+//        log.warn("warn log={}", member.toString());
+//        log.error(" error log={}", member.toString());
+//        memberService.createMember(member);
+        return "redirect:/member";
     }
 }
--- src/main/java/com/bluebird/bluebird/domain/Member.java
+++ src/main/java/com/bluebird/bluebird/domain/Member.java
@@ -6,21 +6,24 @@ import lombok.*;
 import java.util.Date;
 
 @Entity
-@Getter //@Setter
+@ToString
+@Getter @Setter
 @NoArgsConstructor  //인자 없는 기본 생성자
 @AllArgsConstructor //모든 인자를 가지는 생성자
 @Builder            //빌더 패턴
 public class Member {
 
+    //    @GeneratedValue
     @Id //pk 필드
-    @GeneratedValue
     private String id;
     private String pwd;
+    private String name;
+    @Temporal(TemporalType.DATE)
+    private Date birthDay;
+    private String email;
+    private String phoneNum;
     @Temporal(TemporalType.TIMESTAMP)
     private Date joinDate;
-    private String frtName;
-    private String ltName;
-    private String mailAccept;
     @Enumerated(EnumType.STRING)
     private Status status;
     private String profile;
